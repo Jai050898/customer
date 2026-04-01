5ae474bd9290e52a756f4b89dadd3134a67747ce
Harish90151 <jaisuryan.jaswal@devouttechconsultants.com>
2026-04-01 16:12:29 +0530

    add sitepath index

--- yousendit/sample/Authentication.php
+++ yousendit/sample/Authentication.php
@@ -11,8 +11,9 @@
 * governing permissions and limitations.
 *********************************************************************************************/
 
-require_once('include/RestTransport.php');
-require_once('include/ResponseEntities.php');
+require_once(SITEPATH. '/include/RestTransport.php');
+require_once(SITEPATH.'/include/ResponseEntities.php');
+
 
 
 class Authentication{
